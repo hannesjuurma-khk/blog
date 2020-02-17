b47f29ce55537c87f5b4c4280cb8cf584105421b
hannesjuurma <hannes.juurma@khk.ee>
2020-02-17 12:44:48 +0200

    Post design

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,10 +1,10 @@
 <?php require_once APPROOT.'/views/inc/header.php'; ?>
 <?php message('post_message'); ?>
     <div class="row mb-3">
-        <div class="col-md-6">
+        <div class=" pl-4">
             <h1>Posts</h1>
         </div>
-        <div class="col-md-6">
+        <div class="pt-2 pl-4">
             <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">Add Post</a>
         </div>
     </div>
@@ -13,7 +13,7 @@
         <h3 class="card-title"><?php echo $post->post_title; ?></h3>
         <div class="bg-light p-2 mb-3">Created by <?php echo $post->user_id; ?> at <?php echo $post->post_created; ?></div>
         <p class="card-text"><?php echo $post->post_content; ?></p>
-        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id;?>" class="btn btn-info">More</a>
+        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id;?>" class="w-25 btn btn-info">More</a>
     </div>
 <?php endforeach; ?>
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
