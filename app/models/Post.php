caea3ff349e22617e8bc2dee000cdae08a2109fd
hannesjuurma <hannes.juurma@khk.ee>
2020-02-17 12:36:30 +0200

    Posts added

--- app/models/Post.php
+++ app/models/Post.php
@@ -10,10 +10,60 @@ class Post
         $this->db = new Database();
     }
 
-    public function getAllPosts(){
-        $this->db->query('SELECT * FROM posts');
+    public function getPosts(){
+        $this->db->query('SELECT *,
+                            posts.post_id as postId,
+                            users.user_id as userId,
+                            posts.post_created as postCreated
+                            FROM posts
+                            INNER JOIN users
+                            ON posts.user_id = users.user_id
+                            ORDER BY posts.post_created DESC');
         $posts = $this->db->getAll();
         return $posts;
+    }
 
+    public function getPostById($id){
+        $this->db->query('SELECT * FROM posts WHERE post_id=:id');
+        $this->db->bind(':id', $id);
+        $post = $this->db->getOne();
+        return $post;
+    }
+
+    public function editPost($data){
+        $this->db->query('UPDATE posts SET post_title=:title, post_content=:content WHERE post_id=:id');
+        $this->db->bind(':id', $data['id']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':content', $data['content']);
+        $result = $this->db->execute();
+        if($result){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function deletePost($id){
+        $this->db->query('DELETE FROM posts WHERE post_id=:id');
+        $this->db->bind(':id', $id);
+        $result = $this->db->execute();
+        if($result){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function addPost($data){
+        $this->db->query('INSERT INTO posts (post_title, user_id, post_content) VALUES(:title, :user_id, :content)');
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':content', $data['content']);
+        $result = $this->db->execute();
+        if($result){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,20 +1,19 @@
-<?php
-require_once APPROOT.'/views/inc/header.php';
-?>
-
-<h1><?php echo $data['title'];?></h1>
 <?php require_once APPROOT.'/views/inc/header.php'; ?>
-<h1>Posts</h1>
-
+<?php message('post_message'); ?>
+    <div class="row mb-3">
+        <div class="col-md-6">
+            <h1>Posts</h1>
+        </div>
+        <div class="col-md-6">
+            <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">Add Post</a>
+        </div>
+    </div>
 <?php foreach ($data['posts'] as $post) : ?>
     <div class="card card-body mb-3">
         <h3 class="card-title"><?php echo $post->post_title; ?></h3>
         <div class="bg-light p-2 mb-3">Created by <?php echo $post->user_id; ?> at <?php echo $post->post_created; ?></div>
         <p class="card-text"><?php echo $post->post_content; ?></p>
+        <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id;?>" class="btn btn-info">More</a>
     </div>
 <?php endforeach; ?>
-
-
-<?php
-require_once APPROOT.'/views/inc/footer.php';
-?>
+<?php require_once APPROOT.'/views/inc/footer.php'; ?>
